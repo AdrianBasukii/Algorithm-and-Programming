4364b3b28869af05e3b1ad074b66b8fc82451320
Adrian <adriannugrohobasuki@gmail.com>
2023-11-24 22:39:34 +0700

    a

--- Forum Week9/try it yourself/13-1 (Stars)/main.py
+++ Forum Week9/try it yourself/13-1 (Stars)/main.py
@@ -6,13 +6,19 @@ def main():
     pygame.init()
     display = pygame.display.set_mode((800, 600))
     bg_color = 	(0, 0, 0)
-    pygame.display.set_caption("Space Shooters")
-
+    pygame.display.set_caption("Test")
+    image = pygame.image.load('Images/star.bmp')
+    image_rect = image.get_rect()
+    image_rectx = image_rect.width
+    image_recty = image_rect.height
+    x = float(image_rectx)
     display_rect = display.get_rect()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        display.blit(image, image_rect)
 
 main()
+        
